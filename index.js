86160541f6bcd15da34be0cca6f31cd5b2b26315
Rupesh Prajapati <39094963+Rupeshpra22@users.noreply.github.com>
2020-09-06 20:27:13 +0530

    NavBar UI

---
+++ index.js
@@ -0,0 +1,33 @@
+const navLinks = document.querySelector('ul');
+const nav = document.querySelector('nav');
+const hamburger = document.querySelector('.hamburger');
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+let isOpen = false;
+
+
+hamburger.addEventListener('click', () => {
+    if (!isOpen) {
+        hamburgerBtn.classList.add('open');
+        navLinks.classList.add('navbar-open');
+        nav.style.height="185px";
+        isOpen = true;
+    } else {
+        hamburgerBtn.classList.remove('open');
+        navLinks.classList.remove('navbar-open');
+        isOpen = false;
+        nav.style.height="0px";
+    }
+})
+
+removeNavbarOpenClass = () => {
+    // Get the width of the screen
+    let screenWidth = window.innerWidth;
+    if (screenWidth > 600) {
+        hamburgerBtn.classList.remove('open');
+        navLinks.classList.remove('navbar-open');
+        nav.style.height="auto";
+        isOpen = false;
+    }
+}
+window.addEventListener('resize', removeNavbarOpenClass);
+
